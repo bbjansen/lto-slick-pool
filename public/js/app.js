32374f537b5565e7eb257045c4f9b785ac4cc485
bbjansen <bobjansen@pm.me>
2019-03-05 17:05:30 +0100

    leases cant be a fraction silly poo

--- public/js/app.js
+++ public/js/app.js
@@ -113,7 +113,7 @@ Promise.all([balance, leaser, height]).then(function(data) {
   document.getElementById('totalAmountUnpaid').innerText = totalUnpaid.toFixed(2)
   document.getElementById('totalAmountPaid').innerText = totalPaid.toFixed(2)
   document.getElementById('totalAmount').innerText = totalAmount.toFixed(2)
-  document.getElementById('totalLease').innerText = totalLease.toFixed(2)
+  document.getElementById('totalLease').innerText = totalLease
   document.getElementById('blockIndex').innerText = blockIndex
 })
 .catch(function(err) {
