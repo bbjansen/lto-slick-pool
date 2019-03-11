fe6163b3c640fe2c1c015dbae466aba44a2a9ede
bbjansen <bobjansen@pm.me>
2019-03-11 23:09:48 +0100

    block chart bug - wrong target var

--- public/js/network.js
+++ public/js/network.js
@@ -54,7 +54,7 @@ function getLastBlock(blockTable, blockChart) {
       blockChart.data.labels.unshift(data.blocks.timestamp)
       blockChart.data.datasets[0].data.unshift(data.blocks.size)
       blockChart.data.datasets[1].data.unshift(data.blocks.fee)
-      blockChart.data.datasets[2].data.unshift(block.consensus.target)
+      blockChart.data.datasets[2].data.unshift(data.consensus.target)
     
       // Update Count
     }
