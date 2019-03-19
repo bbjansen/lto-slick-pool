36d4959bc93e59aa08c83f7cf8428709cb0c7a98
bbjansen <bobjansen@pm.me>
2019-03-19 19:39:27 +0100

    block table ui improvement + luck ratio

--- public/js/blocks.js
+++ public/js/blocks.js
@@ -14,6 +14,8 @@ function getBlocks() {
     // If data exists...
     if(data.length >= 1) {
 
+      https://explorer.lto.network/blocks/'
+
       // Intialize Tabulator
       new Tabulator('#producedBlocks', {
         data:data,
@@ -25,8 +27,12 @@ function getBlocks() {
         paginationSize: 15,
         initialSort: [{ column:'blockIndex', dir:'desc' }],
         columns: [
-          { title: 'Block', field: 'blockIndex', align: 'left', formatter: 'link', formatterParams: {
-                url: function (row) { return 'https://explorer.lto.network/blocks/' + row._cell.value
+          {
+            title: 'Block', field: 'blockIndex', align: 'center', formatter: function (row) {
+              if(row.getData().paid === 1) {
+                return '<div class="tag is-success is-medium tooltip is-tooltip-right" data-tooltip="Paid" style="width:80px;">' + row._cell.value + '</div>'
+              } else {
+                return '<div class="tag is-warning is-medium tooltip is-tooltip-right" data-tooltip="Unpaid" style="width:80px;">' + row._cell.value + '</div>'
               }
             }
           },
@@ -42,21 +48,43 @@ function getBlocks() {
                 maximumFractionDigits: 2
               })
           }},
-          {
-            title: 'Verified', field: 'verified', align: 'center', formatter: function (row) {
-              if(row._cell.value === 1) {
-                return '<div class="tag is-success is-medium" style="width:80px;">verified</div>'
-              } else {
-                return '<div class="tag is-warning is-medium" style="width:80px;">unverified</div>'
-              }
+          { title: 'Luck Ratio', align: 'left', formatter: function(cell, formatter) {
+            let fee = cell.getData().fee
+            let reward = cell.getData().reward
+            let luck = (reward / fee).toFixed(1)
+            let color
+
+            if(isNaN(luck)) {
+              luck = 0
+              color = 'danger'
             }
-          },
+
+            if(luck < 1) {
+              color = 'danger'
+            }
+            else if(luck < 3) {
+              color = 'warning'
+            }
+            else if(luck < 6) { 
+              color = 'success'
+            }
+            else if(luck < 9) { 
+              color = 'info'
+            }
+            else {
+              color = 'primary'
+            }
+
+            return '<span class="tag is-' + color + ' is-medium" style="width:80px;">' + luck + '</span>'
+          }},
           {
-            title: 'Paid', field: 'paid', align: 'center', formatter: function (row) {
+            title: 'Maturity', field: 'verified', align: 'center', formatter: function (row) {
+              let left = moment.duration(moment().diff(moment(row.getData().timestamp))).asMinutes().toFixed(0)
+
               if(row._cell.value === 1) {
-                return '<div class="tag is-success is-medium" style="width:80px;">paid</div>'
+                return '<div class="tag is-link is-medium" style="width:80px;">90/90</div>'
               } else {
-                return '<div class="tag is-warning is-medium" style="width:80px;">unpaid</div>'
+                return '<div class="tag is-info is-medium" style="width:80px;">' + left + '/90</div>'
               }
             }
           },
